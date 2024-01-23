633898463b321e14fdb24f84ea86298eaba2c1c4
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-01-23 17:36:21 +0100

    Added tests for Hit and isSunk functions

--- Tests/Ship.test.js
+++ Tests/Ship.test.js
@@ -3,7 +3,25 @@ const Ship = require('../Scripts/Ship')
 
 test ('Ship factory creates a ship object with given length', () => {
     let ship1 = Ship(4)
-    expect(ship1).toEqual(
-        {length: 4, hits: 0, sunk: false}
-    )
+    expect(ship1.length).toBe(4)
+    expect(ship1.hits).toBe(0)
+    expect(ship1.sunk).toBe(false)
+});
+
+
+test ('Hit function increments the hit count of the ship', () => {
+    let ship1 = Ship(4)
+    ship1.hit()
+    expect(ship1.hits).toBe(1)
+});
+
+test ('isSunk calculates if the ship is sunk and updates and returns the sunk property', () => {
+    let ship1 = Ship(2)
+    expect(ship1.isSunk()).toBe(false);
+
+    ship1.hit()
+    expect(ship1.isSunk()).toBe(false);
+
+    ship1.hit()
+    expect(ship1.isSunk()).toBe(true);
 });
