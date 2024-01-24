4f27468d7dd049b7ccc339c2cac0c404aba871bd
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-01-24 17:05:06 +0100

    Added allSunk

--- src/Scripts/Gameboard.js
+++ src/Scripts/Gameboard.js
@@ -1,50 +1,68 @@
 const Gameboard = (() => {
-    const createBoard = () => {
-        const board = [];
-        for (let i = 0; i < 10; i++) {
-          board[i] = [];
-          for (let j = 0; j < 10; j++) {
-            board[i][j] = '';
-          }
+  const createBoard = () => {
+    const board = [];
+    for (let i = 0; i < 10; i++) {
+      board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        board[i][j] = '';
+      }
+    }
+    return board;
+  }
+
+  let board = createBoard()
+
+  const getBoard = () => {
+    return board
+  }
+
+  const resetBoard = () => {
+    for (let i = 0; i < 10; i++) {
+      board[i] = [];
+      for (let j = 0; j < 10; j++) {
+        board[i][j] = '';
+      }
+    }
+  }
+
+  const placeShip = (ship, coordinates, direction) => {
+    for (let i = 0; i < ship.length; i++) {
+      if (direction === 'right') {
+        board[coordinates[0]][coordinates[1] + i] = ship
+      } else if (direction === 'down') {
+        board[coordinates[0] + i][coordinates[1]] = ship
+      }
+    }
+  }
+
+  const receiveAttack = (coordinates) => {
+    if (board[coordinates[0]][coordinates[1]] instanceof Object) {
+      board[coordinates[0]][coordinates[1]].hit();
+    } else {
+      board[coordinates[0]][coordinates[1]] = 'X';
+    }
+  }
+
+  const allSunk = () => {
+    let allSunk = true
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 10; j++) {
+        if (typeof board[i][j] === 'object' && board[i][j].isSunk() === false) {
+        allSunk = false;
         }
-        return board;
+      }
     }
+    return allSunk
+  }
 
-    const initBoard = () => {
-        let board = createBoard()
-    }
-
-    initBoard();
-
-    const getBoard = () => {
-        return board
-    }
-
-    const placeShip = (ship, coordinates, direction) => {
-        for (let i = 0; i < ship.length; i++) {
-            if (direction === 'right') {
-                board[coordinates[0]][coordinates[1] + i] = ship
-            } else if(direction === 'down'){
-                board[coordinates[0] + i][coordinates[1]] = ship
-            }
-        }
-    }
-
-    const receiveAttack = (coordinates) => {
-        if (board[coordinates[0]][coordinates[1]] instanceof Object) {
-            board[coordinates[0]][coordinates[1]].hit();
-        } else {
-            board[coordinates[0]][coordinates[1]] = 'X';
-        }
-    }
-
-    return {
-        createBoard,
-        initBoard,
-        getBoard,
-        placeShip,
-        receiveAttack
-    }
+  return {
+    createBoard,
+    resetBoard,
+    getBoard,
+    placeShip,
+    receiveAttack,
+    allSunk
+  }
 })();
 
 module.exports = Gameboard;
--- src/Tests/Gameboard.test.js
+++ src/Tests/Gameboard.test.js
@@ -1,6 +1,10 @@
 const Gameboard = require('../Scripts/Gameboard')
 const Ship = require('../Scripts/Ship')
 
+beforeEach(() => {
+    Gameboard.resetBoard();
+});
+
 test('Creates a 10x10 gameboard', () => {
     let myBoard = Gameboard.createBoard();
     expect(myBoard.length).toBe(10);
@@ -10,7 +14,7 @@ test('Creates a 10x10 gameboard', () => {
           expect(myBoard[i][j]).toBe('');
         }
     }
-})
+});
 
 test('Places ship at specific coordinates', () => {
     let myShip = Ship(3);
@@ -24,12 +28,11 @@ test('Places ship at specific coordinates', () => {
     expect(board[2][3]).toBe(myShip)    
     expect(board[3][3]).toBe(myShip)    
     expect(board[4][3]).toBe(myShip)    
-})
+});
 
 test('takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the "hit" function to the correct ship', () => {
     let myShip = Ship(2);
     Gameboard.placeShip(myShip, [4,5], 'right');
-    let board = Gameboard.getBoard();
 
     Gameboard.receiveAttack([4,5]);
     expect(myShip.hits).toBe(1)
@@ -37,15 +40,44 @@ test('takes a pair of coordinates, determines whether or not the attack hit a sh
     Gameboard.receiveAttack([4,6]);
     expect(myShip.hits).toBe(2)
     expect(myShip.isSunk()).toBe(true)
-})
+});
 
 test('keep track of missed attacks', () => {
     let myShip = Ship(4);
-    let board = Gameboard.createBoard();
-    board = Gameboard.getBoard();
+    
     Gameboard.placeShip(myShip, [8,5], 'right');
     Gameboard.receiveAttack([8,3]);
+    let board = Gameboard.getBoard()
 
     expect(myShip.hits).toBe(0);
     expect(board[8][3]).toBe('X');
+});
+
+describe('report whether or not all of their ships have been sunk', () => {
+    test('no ships sunk', () => {
+        const ship1 = Ship(2);
+        const ship2 = Ship(3);
+        Gameboard.placeShip(ship1, [3,4], 'right');
+        Gameboard.placeShip(ship2, [5,6], 'down');
+        expect(Gameboard.allSunk()).toBe(false);
+    })
+    test('a ship sunk', () => {
+        const ship1 = Ship(2);
+        const ship2 = Ship(3);
+        Gameboard.placeShip(ship1, [3,4], 'right');
+        Gameboard.placeShip(ship2, [5,6], 'down');
+        ship1.hits = 2;
+        expect(ship1.isSunk()).toBe(true);
+        expect(Gameboard.allSunk()).toBe(false);
+    })
+    test('all ships sunk', () => {
+        const ship1 = Ship(2);
+        const ship2 = Ship(3);
+        Gameboard.placeShip(ship1, [3,4], 'right');
+        Gameboard.placeShip(ship2, [5,6], 'down');
+        ship1.hits = 2;
+        ship2.hits = 3;
+        expect(Gameboard.allSunk()).toBe(true);
+    })
+
 })
