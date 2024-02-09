3b9edf24a430b60b100b0e8fbb740bc8a9eabacf
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-02-09 12:27:00 +0100

    Hover higlight

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import css from './style.css';
 import GameController from './Scripts/GameController';
-import ScreenController from './Scripts/ScreenController';
-import Player from './Scripts/Player';
+import css from './style.css';
 
 GameController.game();
 
