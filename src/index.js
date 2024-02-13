a34007f2bf46fcc062ace234920de0da2647a4df
emiroquai <22440776+emiroquai@users.noreply.github.com>
2024-02-13 14:57:38 +0100

    Create deploy branch

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import GameController from './Scripts/GameController.js';
-import css from './style.css';
-
-GameController.game();
-
-const shuffleBtn = document.querySelector('#shuffleBtn');
-shuffleBtn.onclick = GameController.shuffleHumanBoard;
-
